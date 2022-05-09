43bfc7440a99fa83cf92999a46e730131393e174
enigbe <ochekliyeenigbe@gmail.com>
2022-05-09 12:14:56 +0100

    inital project/server setup with test endpoint

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use cosign::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    println!("Starting bitcoin-cosign server on port: {}", port);
+
+    run(listener)?.await
+}
