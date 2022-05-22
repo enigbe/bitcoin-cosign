0c57623235e650463e285d504b4d140f273d1698
enigbe <ochekliyeenigbe@gmail.com>
2022-05-22 05:40:08 +0100

    feat(masterkeys): save and return single masterkeys record

--- src/routes/services/masterkeys.rs
+++ src/routes/services/masterkeys.rs
@@ -12,19 +12,22 @@ pub struct RequestNetwork {
 struct MasterKeysResponse {
     msg: String,
     status: u16,
+    data: Option<MasterKeys>,
 }
 
+#[derive(Debug, serde::Serialize)]
 pub struct MasterKeys {
     pub master_xpriv: String,
     pub master_xpub: String,
 }
 
-/// Generate service master keys and save a record to the database
-/// The request body: e.g. {"network": "bitcoin"}
+/// Return a record of the saved masterkeys if they exist or generate
+/// and return service master keys and save a record to the database
+/// The request body contains the network: e.g. {"network": "bitcoin"}
+///
 /// TODO: Authenticate this endpoint so that only internal requests from
 /// authenticated staff are authorized to call this endpoint
 pub async fn masterkeys(req: web::Json<RequestNetwork>, pool: web::Data<PgPool>) -> HttpResponse {
-    // 1. Generate service master keys
     let network = match req.network.as_str() {
         "bitcoin" => Network::Bitcoin,
         "regtest" => Network::Regtest,
@@ -34,58 +37,53 @@ pub async fn masterkeys(req: web::Json<RequestNetwork>, pool: web::Data<PgPool>)
             let rsp = MasterKeysResponse {
                 msg: "ERROR: Invalid network. Enter one of 'bitcoin', 'regtest', 'testnet', 'signet'.".to_string(),
                 status: StatusCode::BAD_REQUEST.as_u16(),
+                data: None,
             };
             return HttpResponse::BadRequest().json(rsp);
         }
     };
 
-    let masterkeys = generate_service_master_keys(network);
-
-    // 2. Check uniqueness of service master keys. If not unique, generate new keys
-    let existing_masterkeys = find_saved_service_masterkeys(&pool, &masterkeys).await;
-
+    // 1. Check DB for saved masterkeys
+    let existing_masterkeys = find_saved_service_masterkeys(&pool, &network).await;
     match existing_masterkeys {
-        Ok(_key) => {
+        Ok(keys) => {
+            // 2. Return if they exist
+
+            let rsp_msg = MasterKeysResponse {
+                msg: "SUCCESS: Existing masterkeys in database".to_string(),
+                status: StatusCode::OK.as_u16(),
+                data: Some(keys),
+            };
+            return HttpResponse::Ok().json(rsp_msg);
+        }
+        Err(e) => {
+            // 3. Generate, save, and return masterkeys if they don't
+            println!("ERROR: {}", e);
             // 1. Generate new service keys
             let new_masterkeys = generate_service_master_keys(network);
             // 2. Save them to the database and return
             match insert_service_masterkeys(&pool, &new_masterkeys).await {
                 Ok(_) => {
+                    let masterkeys = MasterKeys {
+                        master_xpriv: new_masterkeys.xpriv,
+                        master_xpub: new_masterkeys.xpub,
+                    };
                     let rsp_msg = MasterKeysResponse {
                         msg: "SUCCESS: Master extended keys saved to database".to_string(),
                         status: StatusCode::OK.as_u16(),
+                        data: Some(masterkeys),
                     };
-                    return HttpResponse::BadRequest().json(rsp_msg);
+                    return HttpResponse::Ok().json(rsp_msg);
                 }
                 Err(e) => {
                     let rsp_msg = MasterKeysResponse {
                         msg: format!("ERROR: Error querying for master keys {:?}", e),
                         status: StatusCode::BAD_REQUEST.as_u16(),
+                        data: None,
                     };
                     return HttpResponse::BadRequest().json(rsp_msg);
                 }
             }
-        }
-        Err(e) => {
-            println!("{}", e);
-        }
-    }
-
-    // 3. Save to database
-    match insert_service_masterkeys(&pool, &masterkeys).await {
-        Ok(_) => {
-            let rsp_msg = MasterKeysResponse {
-                msg: "SUCCESS: Master extended keys saved to database".to_string(),
-                status: StatusCode::OK.as_u16(),
-            };
-            return HttpResponse::Ok().json(rsp_msg);
-        }
-        Err(e) => {
-            let rsp_msg = MasterKeysResponse {
-                msg: format!("ERROR: Error querying for master keys {:?}", e),
-                status: StatusCode::BAD_REQUEST.as_u16(),
-            };
-            return HttpResponse::BadRequest().json(rsp_msg);
         }
     }
 }
@@ -97,16 +95,15 @@ pub async fn masterkeys(req: web::Json<RequestNetwork>, pool: web::Data<PgPool>)
 ///     service_masterkeys (&ServiceMasterKeys): A shared reference to a ServiceMasterKeys instance
 pub async fn find_saved_service_masterkeys(
     pool: &PgPool,
-    service_masterkeys: &ServiceMasterKeys,
+    network: &Network,
 ) -> Result<MasterKeys, sqlx::Error> {
     let keys = sqlx::query_as!(
         MasterKeys,
         r#"
         SELECT master_xpriv, master_xpub FROM service_keys 
-        WHERE master_xpriv = ($1) and master_xpub = ($2)
+        WHERE network = ($1)
         "#,
-        service_masterkeys.xpriv,
-        service_masterkeys.xpub
+        format!("{}", network),
     )
     .fetch_one(pool)
     .await?;
@@ -125,10 +122,11 @@ pub async fn insert_service_masterkeys(
 ) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
-        INSERT INTO service_keys (mnemonic, master_xpriv, master_xpub)
-        VALUES ($1, $2, $3)
+        INSERT INTO service_keys (mnemonic, network, master_xpriv, master_xpub)
+        VALUES ($1, $2, $3, $4)
         "#,
         format!("{}", service_masterkeys.mnemonic),
+        format!("{}", service_masterkeys.network),
         service_masterkeys.xpriv,
         service_masterkeys.xpub
     )
