aa57b13474f5db8eace472fbe56b0c815e2b8137
tvpeter <tintin@PeterTyonumAir.lan>
2022-05-27 11:12:49 +0100

    fix validating user supplied address

--- src/domain/address.rs
+++ src/domain/address.rs
@@ -1,14 +1,20 @@
+use std::str::FromStr;
+
+use bitcoincore_rpc::bitcoin::Address;
 use serde::{Serialize, Deserialize};
+
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct UserAddress(String);
 
 impl UserAddress {
-    pub fn validate(address: String) -> Result<UserAddress, String> {
-        if address.len() == 62 {
-            Ok(Self(address))
-        }else {
-            Err(format!("Supplied address is invalid"))
+    pub fn validate(address: String) -> Result<Address, String> {
+        let valid_address = Address::from_str(address.as_str());
+        match valid_address {
+            Ok(addr) =>  Ok(addr),
+            Err(err) => {
+                Err(format!("{} is not a valid address: {}", address, err))
+            }
         }
     }
 }
